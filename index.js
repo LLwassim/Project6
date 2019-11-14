b591442b134c4c95a6d47f421ac9ebe7dcff4556
Wassim Lkorchy <lacorchy77@gmail.com>
2019-11-14 10:04:29 -0500

    express and routers set up. next step add more routes.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const express = require('express')
+const app = express()
+const parser = require('body parser')
+app.use(parser.json)
+const countries = require('./models/Countries')
+
+app.get('/Countries', function(req, res){
+    countries.find({}).then(countries => {
+        res.json(countries)
+    })
+})
+
+app.listen(3000,() => console.log("listening"))
